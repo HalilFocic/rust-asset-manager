1dd3dc08445d989eb7b987e92c230f448097fa16
Halil Focic <halil.focic@zendev.se>
2024-05-20 18:39:45 +0200

    added basic documentation for command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("assetm")
+        .version("1.0")
+        .author("Halill Focic <halil.focic@gmail.com>")
+        .about("Simple asset manager for projects")
+        .subcommand(Command::new("ls").about("Lists all added projects"))
+        .subcommand(
+            Command::new("add")
+                .about("Adds a project and its path to list of projects")
+                .arg(Arg::new("project_name").required(true).index(1))
+                .arg(Arg::new("path_to_project").required(true).index(2)),
+        )
+        .subcommand(
+            Command::new("remove")
+                .about("Removes a project")
+                .arg(Arg::new("project_name").required(true).index(1)),
+        )
+        .subcommand(
+            Command::new("default")
+                .about("Sets the default path from where to pull the asset")
+                .arg(Arg::new("path").required(true).index(1)),
+        )
+        .subcommand(
+            Command::new("import")
+                .about("Imports a file to a project")
+                .arg(Arg::new("project_name").required(true).index(1))
+                .arg(Arg::new("file_name").required(true).index(2)),
+        )
+        .get_matches();
 }
