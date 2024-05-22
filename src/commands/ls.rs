dd863353979f4f652f57f61f6238acf81a01730b
Halil Focic <halil.focic@zendev.se>
2024-05-22 16:38:48 +0200

    feat:implemented better response for ls when no projects are present
    ls response when no projects are found is now colored

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -4,7 +4,10 @@ pub fn handle_ls() {
     let state = ProjectState::load();
     let projects = state.projects();
     if projects.is_empty() {
-        println!("No projects found");
+        println!(
+            "Looks like you have no projects yet. Use {} to add one.",
+            "`assetm add`".bold().bright_green()
+        );
         return;
     }
     for (project_name, _) in projects {
