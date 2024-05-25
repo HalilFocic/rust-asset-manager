477e3646321741121481f87eefa8b4960a47d4f1
Halil Focic <halil.focic@zendev.se>
2024-05-25 16:48:31 +0200

    feat: added commands to manipulate default path
    Commands:
    default -> Prints the default path from where to pull the assets
    set-default -> Changes the default path from where to pull the assets

--- src/commands/default.rs
+++ src/commands/default.rs
@@ -1,4 +1,12 @@
+use crate::project_state;
 use colored::Colorize;
-pub fn handle_default(){
-    println!("{}", "Default path for assets set successfuly".green().bold());
+pub fn handle_default(default_path: &str) {
+    let mut project_state = project_state::ProjectState::load();
+    project_state.set_path(default_path);
+    println!("Default path is set to: {}", default_path.green());
+}
+pub fn print_default() {
+    let project_state = project_state::ProjectState::load();
+    let path = project_state.path();
+    println!("Default path is: {}", path.bright_blue().bold());
 }
